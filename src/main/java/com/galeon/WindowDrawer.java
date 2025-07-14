adbcb933a94897e8bdee063d355a0b1524619cd7
MikhailDolgopolov <mishad2304@gmail.com>
2025-07-14 20:30:36 +0300

    The first quad

--- src/main/java/com/galeon/WindowDrawer.java
+++ src/main/java/com/galeon/WindowDrawer.java
@@ -1,5 +1,8 @@
 package com.galeon;
 
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.awt.GLJPanel;
@@ -22,18 +25,25 @@ public class WindowDrawer implements GLEventListener {
             GLCapabilities caps = new GLCapabilities(profile);
             caps.setDoubleBuffered(true);
 
-            GLJPanel canvas = new GLJPanel(caps);
-            WindowDrawer dw = new WindowDrawer(new SimpleQuadRenderer());
-            canvas.addGLEventListener(dw);
+            GLWindow window = GLWindow.create(caps);
 
-            FPSAnimator animator = new FPSAnimator(canvas, 1, true);
+            FPSAnimator animator = new FPSAnimator(window, 1, true);
             animator.start();
 
-            JFrame frame = new JFrame("Red Quad Test");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(canvas);
-            frame.setSize(800, 600);
-            frame.setVisible(true);
+            // replace JFrame with the window
+            window.setTitle("Red Quad Test");
+            window.setSize(800, 600);
+            window.setUndecorated(false);
+            window.setResizable(true); 
+            window.setVisible(true);
+            window.addGLEventListener(new WindowDrawer(new SimpleQuadRenderer()));
+            window.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowDestroyed(WindowEvent e) {
+                    animator.stop();
+                    System.exit(1);
+                }
+            });
         });
         
     }
